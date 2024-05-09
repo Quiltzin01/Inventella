5f6575c48d06908382f5e6e6039307376f74da06
Quiltzin <quiltzin01@hotmail.com>
2024-05-09 17:45:01 -0600

    Agregados archivos Login.js en las carpetas scr/screens/Admin y scr/screens/Moderador

---
+++ scr/screens/Styles/Styles.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+import React from "react";
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "rgba(255, 255, 255, 0.9)",
+    borderRadius: 25,
+    padding: 29,
+    marginTop: 30,
+    marginBottom: 30,
+  },
+  title: {
+    fontSize: 30,
+    color: "#055A87",
+    marginBottom: 5,
+    textAlign: "center",
+  },
+  text: {
+    fontSize: 15,
+    color: "#000000",
+    marginTop: 3,
+    marginBottom: 1,
+    textAlign: "center",
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: "#1374",
+    borderRadius: 7,
+    padding: 7,
+    marginTop: 6,
+  },
+  button: {
+    fontSize: 30,
+    color: "#055A87",
+    marginBottom: 5,
+    textAlign: "center",
+  },
+});
+
+export default styles;
