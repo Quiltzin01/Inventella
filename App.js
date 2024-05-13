9cc4d39404c258bb79766c13f874a53bd46b76be
franklinramon <140756834+franklinramon@users.noreply.github.com>
2024-05-13 00:30:33 +0000

    loginscreen

--- App.js
+++ App.js
@@ -4,14 +4,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { config } from "@gluestack-ui/config";
 import { GluestackUIProvider } from "@gluestack-ui/themed";
-
+import LoginScreen from "./scr/screens/User/LoginScreen";
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
     <GluestackUIProvider config={config}>
       <NavigationContainer>
         <StatusBar style="auto" />
-        <Stack.Navigator initialRouteName = "UsuarioBase" >
+
+        <Stack.Navigator initialRouteName = "usuariobase" >
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
           <Stack.Screen name="UserHome" component={UserHome} />
             <Stack.Screen name="Alimentos" component={AlimentosButton} />
