7c8d8f3da850b7a07214db9d796457b622d8f9fc
franklinramon <140756834+franklinramon@users.noreply.github.com>
2024-05-13 01:37:11 +0000

    App

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { config } from "@gluestack-ui/config";
 import { GluestackUIProvider } from "@gluestack-ui/themed";
 import LoginScreen from "./scr/screens/User/LoginScreen";
 import UserHome from "./scr/screens/User/UserHome";
+import AlimentosButton from "./scr/screens/User/AlimentosButton";
+
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
     <GluestackUIProvider config={config}>
       <NavigationContainer>
         <StatusBar style="auto" />
-        <Stack.Navigator initialRouteName = "LoginScreen" component={LoginScreen} >
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Navigator initialRouteName="LoginScreen">
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
           <Stack.Screen name="UserHome" component={UserHome} />
-            <Stack.Screen name="Alimentos" component={AlimentosButton} />
+          <Stack.Screen name="AlimentosButton" component={AlimentosButton} />
         </Stack.Navigator>
       </NavigationContainer>
     </GluestackUIProvider>
